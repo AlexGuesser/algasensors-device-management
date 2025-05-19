e9f4eeec9820ff3d6dd5eed51ad10efe7b6e3571
Axonify-Alex-Guesser <140539263+Axonify-Alex-Guesser@users.noreply.github.com>
2025-05-19 10:32:24 -0300

    Adding spring boot projects

---
+++ src/test/java/dev/alexguesser/devicemanagement/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexguesser.devicemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
